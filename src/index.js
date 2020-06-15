71cb3b79f13f529f05a44e74ce7fa149059b4de1
pilikiwi <pilikiwi@gmail.com>
2020-06-14 20:45:00 -0700

    1.5: course information step5

--- src/index.js
+++ src/index.js
@@ -2,28 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <div>
-      <h1>{course}</h1>
-      <p>{`${parts[0].name} ${parts[0].exercises}`}</p>
-      <p>{parts[1].name +" "+parts[1].exercises}</p>
-      <p>{parts[2].name} {parts[2].exercises}</p>
-      <p>{`Number of exercises ${parts[0].exercises + parts[1].exercises+ parts[2].exercises}`}</p>
+      <h1>{course.name}</h1>
+      <p>{`${course.parts[0].name} ${course.parts[0].exercises}`}</p>
+      <p>{`${course.parts[1].name} ${course.parts[1].exercises}`}</p>
+      <p>{`${course.parts[2].name} ${course.parts[2].exercises}`}</p>
+      <p>Number of exercises {`${course.parts[2].exercises + course.parts[0].exercises +
+         course.parts[1].exercises}`}</p>
     </div>
   )
 }
